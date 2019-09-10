dec07f260da365ee2caeb344a68f5d7e85950bbd
Melissa Weber Mendonça <melissawm@gmail.com>
2019-09-10 14:57:40 -0300

    Improving on pointers

--- course1/week1/karatsuba_multiplication.c
+++ course1/week1/karatsuba_multiplication.c
@@ -8,15 +8,14 @@ char * string_pad(char *s, int n, char side);
 
 int main() {
 
-  /* Variable declarations */
+  // Variable declarations
   int n, m, half;
 
-  /* Numbers we wish to multiply */
-  char pi[65];
-  char exp[65];
+  // Numbers we wish to multiply
+  // Why can't we do char pi[4] = "3141";?
+  char pi[] = "3141"; //592653589793238462643383279502884197169399375105820974944592";
+  char exp[] = "2718"; //281828459045235360287471352662497757247093699959574966967627";
 
-  strcpy(pi, "3141592653589793238462643383279502884197169399375105820974944592");
-  strcpy(exp, "2718281828459045235360287471352662497757247093699959574966967627");
   n = strlen(pi);
   m = strlen(exp);
 
@@ -28,52 +27,95 @@ int main() {
   half = ceil(n/2);
   printf("half = %d\n", half);
 
-  char *result = string_operate(pi, exp, 'B');
+  // Let's first try to add the two strings
+  char * result = string_operate(pi, exp, 'A');
   printf("result = %s\n", result);
 
-  char* result_padded = (char*)malloc(strlen(pi)+n+3);
-  result_padded = string_pad(pi, 3, 'L');
+  //char * result_padded = (char*)malloc(2*n+3);
+//  char * result_padded = string_pad(pi, 3, 'L');
 
   /* To compare: strncmp(string1, string2, nbcharstobecompared) */
   
   return 0;
 }
 
-char *string_operate(char *s, char *r, char op) {
-    char* result = (char*)malloc(strlen(s) + strlen(r)+3);//new string with enough space for both and \0
+char * string_operate(char * s, char * r, char op) {
+    int lens = strlen(s);
+    int lenr = strlen(r);
+    char * result = (char*)malloc(lens + lenr + 1);//new string with enough space for both and \0
+
+    if (lens > lenr) {
+        // Pad r with zeros to the left
+        strcpy(result, r);
+        string_pad(result, lens-lenr, 'L');
+        // r = pad(r, lens-lenr)
+    } else if (lens < lenr) {
+        // Pad s with zeros to the left
+        strcpy(result, s);
+        string_pad(result, lenr-lens, 'L');
+        // s = pad(s, lenr-lens)
+    }
+
     if (op == 'A') {
         // we are adding the two strings
-        //printf("s = %s\n", s);
         strcpy(result, s);
-    } else if (op == 'S') {
-        // we are subtracting the two strings
-        //printf("r = %s\n", r);
-        strcpy(result, r);
-    } else {
-        printf("Warning: wrong operation for strings.\n");
-        // strncat(dest, src, howmanychars)
-        strcpy(result, r);
-        strncat(result, s, strlen(result));
+        strcat(result, r);
+//    } else if (op == 'S') {
+//        // we are subtracting the two strings
+//        //printf("r = %s\n", r);
+//        strcpy(result, r);
+//    } else {
+//        printf("Warning: wrong operation for strings.\n");
+//        // strncat(dest, src, howmanychars)
+//        strcpy(result, r);
+//        //strncat(result, s, strlen(result));
     }
     return result;
+////
+////    carry = 0
+////    result = ""
+////    if op=="add":
+////    for i in range(max(lens, lenr)-1, -1, -1):
+////    partial = str(int(s[i])+int(r[i])+carry)
+////    if len(partial) == 2:
+////    carry = int(partial[0])
+////    else:
+////    carry = 0
+////    result = partial[-1] + result
+////    elif op=="subtract":
+////    for i in range(max(lens, lenr)-1, -1, -1):
+////    if int(s[i])-carry >= int(r[i]):
+////    partial = str(int(s[i])-carry-int(r[i]))
+////    carry = 0
+////    else:
+////    partial = str(10+(int(s[i])-carry)-int(r[i]))
+////    carry = 1
+////    result = partial[-1] + result
+////    else:
+////    raise("Error: wrong string operation!")
+////
+////    if carry != 0:
+////    result = str(carry)+result
+////    return result
 }
 
 char * string_pad(char *s, int n, char side) {
     int i;
-    char temp[strlen(s)+n+1];
     char zeros[n];
-    for (i=1; i<=n; i++) {
-        strncat(zeros, "0", 2);
+    printf("n = %d\n", n);
+    for (i=0; i<=n; i++) {
+        zeros[i] = '0';
     }
+    printf("%s\n", zeros);
     if (side == 'L') {
-        strncat(temp, zeros, n);
+        strncat(s, zeros, n);
         strncat(temp, s, strlen(s));
-    } else if (side == 'R') {
-        strncat(temp, s, strlen(s));
-        strncat(temp, zeros, n);
-    } else {
-        printf("Wrong padding option");
+//    } else if (side == 'R') {
+//        strncat(temp, s, strlen(s));
+//        strncat(temp, zeros, n);
+//    } else {
+//        printf("Wrong padding option");
     }
     printf("apos padding: %s\n", temp);
-    return temp;
+    return s;
 }
